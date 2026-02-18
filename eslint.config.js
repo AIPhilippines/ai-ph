275e68479fb14ab2beded4b0bc348e78548e196f
gentdimad <gentdimad@gmail.com>
2026-02-18 23:53:08 +0800

    more lenient rules for the linter

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,12 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-empty': 'off',
+      'no-useless-escape': 'off',
+      'react-refresh/only-export-components': 'off',
+    }
   },
 ])
